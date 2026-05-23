4ffed646a168078cddaf28e82399bfff163d7d40
Ian Swartz <ian.s.swartz@gmail.com>
2026-05-23 00:31:33 -0400

    Update model_product.js

--- model_product.js
+++ model_product.js
@@ -1,3 +1,12 @@
+/*
+ * Filename: model_product.js
+ * Authors: Ian Swartz
+ * Creation Date: 2025-11-11
+ * Last Edit Date: 2026-05-23
+ * Class: CMSC 421 Web Development
+ * Description: code for mongodb product schema part of website, based on Dr X's example
+ */
+
 const mongoose = require("mongoose");
 
 // REMOVED THE HARDCODED CONNECT STRING THAT WAS HIJACKING THE ROUTE
